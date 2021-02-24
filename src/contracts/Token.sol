6e011e7ee78004557e12beaf905d68f3b306997e
Pichi Loco <matiasdmansilla@hotmail.com>
2021-02-23 21:19:00 -0300

    first

---
+++ src/contracts/Token.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    address public minter;
+
+  //add minter changed event
+
+  constructor() public payable ERC20("Descentralized Bank Currency", "DCB") {
+      minter = msg.sender;
+  }
+
+  //Add pass minter role function
+
+  function mint(address account, uint256 amount) public {
+    //check if msg.sender have minter role
+		_mint(account, amount);
+	}
+}
